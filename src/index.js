12744da49dc733e304b3743100a66e5a29f76316
Mateusz Sobieraj <core.editor>
2021-09-29 01:12:20 +0200

    Change table color to white

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Reveal from "reveal.js";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/league.css";
 import "reveal.js/plugin/highlight/monokai.css";
+import "./styles.css";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import RevealNotes from "reveal.js/plugin/notes/notes.esm.js";
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
